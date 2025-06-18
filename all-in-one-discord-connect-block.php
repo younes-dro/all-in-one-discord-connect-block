fc99c10db35ceaba2f45211cc44d627f90bb5df9
Younes DRO <31171+younes-dro@users.noreply.github.com>
2025-06-18 19:18:00 +0100

    Add name space `Dro\AIODiscordBlock;` and initialize folder structure

--- all-in-one-discord-connect-block.php
+++ all-in-one-discord-connect-block.php
@@ -15,14 +15,17 @@
  * @package AllInOneDiscordConnectBlock
  */
 
+namespace Dro\AIODiscordBlock;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
 $dro_aio_discord_block_version = get_file_data(
 	__FILE__,
 	array( 'Version' )
 );
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
-}
 /**
  * Registers the block using a `blocks-manifest.php` file, which improves the performance of block type registration.
  * Behind the scenes, it also registers all assets so they can be enqueued
@@ -32,41 +35,26 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
  */
 function dro_aio_discord_block_block_init() {
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` and registers the block type(s)
-	 * based on the registered block metadata.
-	 * Added in WordPress 6.8 to simplify the block metadata registration process added in WordPress 6.7.
-	 *
-	 * @see https://make.wordpress.org/core/2025/03/13/more-efficient-block-type-registration-in-6-8/
-	 */
+
 	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
 		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
 		return;
 	}
-
-	/**
-	 * Registers the block(s) metadata from the `blocks-manifest.php` file.
-	 * Added to WordPress 6.7 to improve the performance of block type registration.
-	 *
-	 * @see https://make.wordpress.org/core/2024/10/17/new-block-type-registration-apis-to-improve-performance-in-wordpress-6-7/
-	 */
 	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
 		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
 	}
-	/**
-	 * Registers the block type(s) in the `blocks-manifest.php` file.
-	 *
-	 * @see https://developer.wordpress.org/reference/functions/register_block_type/
-	 */
+
 	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
 	foreach ( array_keys( $manifest_data ) as $block_type ) {
 		register_block_type( __DIR__ . "/build/{$block_type}" );
 	}
 }
-add_action( 'init', 'dro_aio_discord_block_block_init' );
+add_action( 'init', __NAMESPACE__ . '\\dro_aio_discord_block_block_init' );
 
 /**
  * Replaces placeholders in the block content with actual user data.
+ *
+ * TODO: will repalce discord_username with the actual Discord username
  *
  * @param string $block_content The block content.
  * @param array  $block         The full block, including name and attributes.
@@ -83,4 +71,4 @@ function dro_aio_discord_block_replace_placeholders( $block_content, $block, $in
 
 	return $block_content;
 }
-add_filter( 'render_block_dro-block/all-in-one-discord-connect-block', 'dro_aio_discord_block_replace_placeholders', 10, 3 );
+add_filter( 'render_block_dro-block/all-in-one-discord-connect-block', __NAMESPACE__ . '\\dro_aio_discord_block_replace_placeholders', 10, 3 );
---
+++ includes/interfaces/class-dro-aio-discord-interface.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Interface for Discord integration in All-in-One Discord Connect Block plugin.
+ * This interface defines the methods for each service class ( pmpro, memberpress)
+ */

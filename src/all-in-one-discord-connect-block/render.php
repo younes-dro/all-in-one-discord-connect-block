91c6d204efba30cae8453d9c372587b4ab360ea3
Younes DRO <31171+younes-dro@users.noreply.github.com>
2025-08-05 14:19:31 +0100

    Load Discord user data before rendering block
    Added logic to load Discord user data for the current user if a service is active before rendering the block. This ensures user-specific data is available during block rendering.

--- src/all-in-one-discord-connect-block/render.php
+++ src/all-in-one-discord-connect-block/render.php
@@ -12,15 +12,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 use Dro\AIODiscordBlock\includes\Dro_AIO_Discord_Render as Discord_Render;
 use Dro\AIODiscordBlock\includes\Dro_AIO_Discord_Resolver as Discord_Resolver;
 error_log( print_r( Discord_Resolver::get_active_service(), true ) );
+
+$user_id = get_current_user_id();
+$service = Discord_Resolver::get_active_service();
+
+if ( $service && $user_id ) {
+	$service->load_discord_user_data( $user_id );
+}
+
 $render_block = '';
 try {
-
-	$render_block = Discord_Render::get_instance( Discord_Resolver::get_active_service() )->render( $attributes, $content, $block );
-
+	$render_block = Discord_Render::get_instance( $service )->render( $attributes, $content, $block );
 } catch ( Throwable $e ) {
 	error_log( $e->getMessage() );
 }
 
-error_log( print_r( $render_block, true ) );
 echo $render_block;
+
 // echo wp_kses_post( $render_block );
